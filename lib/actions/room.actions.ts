a0aa570bd073deb5b2e22d932d8cfb37d7df6486
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-17 19:19:43 +0530

    made server action to make liveblocks room

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,53 @@
+'user server';
+
+// utils
+import { revalidatePath } from 'next/cache';
+import { parseStringify } from '../utils';
+
+// nanoid for unique if gen
+import { nanoid } from 'nanoid';
+
+// liveblocks util
+import { liveblocks } from '@/lib/liveblocks';
+
+
+
+// Server action
+export const createDocument = async ({
+  userId, 
+  email 
+}: CreateDocumentParams ) => {
+
+  // create a unique id for each room
+  const roomId = nanoid();
+
+  try {
+    // metadata for room
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: 'Untitled',
+    }    
+
+    // level of access
+    const usersAccesses: RoomAccesses = {
+      [email]: ['room:write']
+    }
+
+    // Creating room
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: [],
+    });
+
+    // revalidate path (so that new document is displayed)
+    revalidatePath('/')
+
+    // return room (whenver returning stuff from server action, parse and stringify it)
+    return parseStringify(room)
+
+  } catch (error) {
+    console.error("Error while creating a room: ", error)
+  }
+}
